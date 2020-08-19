b199a2a9eb2cf7ed8abb2c616b05a9ba5a480d74
Jordi Fierro <jordifierromulero@gmail.com>
2020-08-19 17:50:29 +0200

    Add INTERNAL_IP env var to llaor docker runs

--- llaor/api/setup.sh
+++ llaor/api/setup.sh
@@ -15,10 +15,10 @@ cd llaor-api
 sudo docker build -t llaor/api .
 
 sudo docker volume create llaor-statics-01
-sudo docker run -d --restart=always --env-file env.list --net llaor-net -v llaor-statics-01:/code/llaor/staticfiles --name llaor-api-01 -t llaor/api
+sudo docker run -d --restart=always --env-file env.list --net llaor-net -v llaor-statics-01:/code/llaor/staticfiles -e INTERNAL_IP=127.0.0.2 --name llaor-api-01 -t llaor/api
 sudo docker run --name llaor-nginx-01 -v llaor-statics-01:/usr/share/nginx/html/static:ro -v /etc/nginx/sites-available/api.llaor.com/nginx-01.conf:/etc/nginx/nginx.conf:ro -p 127.0.0.2:80:80 --net llaor-net --restart=always -d nginx
 
 
 sudo docker volume create llaor-statics-02
-sudo docker run -d --restart=always --env-file env.list --net llaor-net -v llaor-statics-02:/code/llaor/staticfiles --name llaor-api-02 -t llaor/api
+sudo docker run -d --restart=always --env-file env.list --net llaor-net -v llaor-statics-02:/code/llaor/staticfiles -e INTERNAL_IP=127.0.0.3 --name llaor-api-02 -t llaor/api
 sudo docker run --name llaor-nginx-02 -v llaor-statics-02:/usr/share/nginx/html/static:ro -v /etc/nginx/sites-available/api.llaor.com/nginx-02.conf:/etc/nginx/nginx.conf:ro -p 127.0.0.3:80:80 --net llaor-net --restart=always -d nginx
